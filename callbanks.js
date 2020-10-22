89ed68f19d1eef6beb6c5391427f677d3064a7bb
andrewemcmanus <60327690+andrewemcmanus@users.noreply.github.com>
2020-10-22 10:19:11 -0400

    checkNumber

---
+++ callbanks.js
@@ -0,0 +1,101 @@
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    }
+    else if (num < 0) {
+        return "negative";
+    }
+    else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(5));
+function checkNumber(num) {
+    if (num > 0) {
+        return "positive";
+    } else if (num < 0) {
+        return "negative";
+    } else {
+        return "zero";
+    }
+}
+console.log(checkNumber(10));
+function checkNumber(num) {
+    if (num > 0) {
+        return "positive";
+    } else if (num < 0) {
+        return "negative";
+    } else {
+        return "zero";
+    }
+}
+console.log(checkNumber(-3));
+function checkNumber(num) {
+    if (num > 0) {
+        return "positive";
+    } else if (num < 0) {
+        return "negative";
+    } else {
+        return "zero";
+    }
+}
+console.log(checkNumber(23));
+function checkNumber(num) {
+    if (num > 0) {
+        return "positive";
+    } else if (num < 0) {
+        return "negative";
+    } else {
+        return "zero";
+    }
+}
+console.log(checkNumber(-76));
+function checkNumber(num) {
+    if (num > 0) {
+        return "positive";
+    } else if (num < 0) {
+        return "negative";
+    } else {
+        return "zero";
+    }
+} 
+console.log(checkNumber(148));
+function checkNumber(num) {
+    if (num > 0) {
+        return "positive";
+    } else if (num < 0) {
+        return "negative";
+    } else {
+        return "zero";
+    }
+}
+console.log(checkNumber(-7431));
+function checkNumber(num) {
+    if (num > 0) {
+        return "positive";
+    } else if (num < 0) {
+        return "negative";
+    } else {
+        return "zero";
+    }
+} console.log(checkNumber(173));
+function checkNumber(num) {
+    if (num > 0) {
+        return "positive";
+    } else if (num < 0) {
+        return "negative";
+    } else {
+        return "zero";
+    }
+} 
+console.log(checkNumber(-3));
+function checkNumber(num) {
+    if (num > 0) {
+        return "positive";
+    } else if (num < 0) {
+        return "negative";
+    } else {
+        return "zero";
+    }
+} 
+console.log(checkNumber(0));
