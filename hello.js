6bbcd76ae9998ca87f6ea32c2b30865728c4e1f9
andrewemcmanus <60327690+andrewemcmanus@users.noreply.github.com>
2020-10-22 09:54:14 -0400

    hello.js

---
+++ hello.js
@@ -0,0 +1,24 @@
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?'
+    return msg;
+}
+console.log(sayHello(('SEI 1019'));
+function sayHello(name) {
+    let msg = 'Hello, ' + name + ". How are you?"
+    return msg;
+}
+console.log(sayHello('Andrew'));
+function sayHello(name) {
+    let msg = 'Hello, ' + name + ". How are you?"
+    return msg;
+}
+console.log(sayHello('General Assembly'));
+function sayHello(name) {
+    let msg = 'Hello, ' + name + ". How are you?"
+    return msg;
+}
+console.log(sayHello("Meredith"));
+function sayHello(name) {
+    let msg = "Hello, " + name + ". How are you?" 
+}
+console.log(sayHello("Andy"));
